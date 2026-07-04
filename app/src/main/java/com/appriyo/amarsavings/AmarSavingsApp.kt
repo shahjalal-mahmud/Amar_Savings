670da36cf1cb9aa497d800d160cb296ca9002ced
shahjalal-mahmud <mahmud.nubtk@gmail.com>
2026-07-04 21:03:21 +0600

    feat(backup): initialize BackupScheduler on app start for auto-backup functionality

--- app/src/main/java/com/appriyo/amarsavings/AmarSavingsApp.kt
+++ app/src/main/java/com/appriyo/amarsavings/AmarSavingsApp.kt
@@ -1,7 +1,9 @@
 package com.appriyo.amarsavings
 
 import android.app.Application
+import com.appriyo.amarsavings.data.backup.BackupScheduler
 import com.appriyo.amarsavings.di.appModule
+import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,5 +14,7 @@ class AmarSavingsApp : Application() {
             androidContext(this@AmarSavingsApp)
             modules(appModule)
         }
+        // Start observing the data layer so the auto-backup pipeline is alive.
+        get<BackupScheduler>().start()
     }
 }
